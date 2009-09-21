8ce2b57ef5121eeacd3a84119fde81b63bce03d4
seb.bacon <seb.bacon@220e2412-9961-11de-a05d-3bd73e269465>
2009-09-21 16:43:29 +0000

    cosmetic changes: add footer, change name of site, different text on homepage

--- invite/strings.py
+++ invite/strings.py
@@ -1,5 +1,5 @@
-INVITE_TEXT = "Join the Constituency Volunteer Network to help out in your constituency for the upcoming election! We're politically neutral and blah blah"
-INVITE_SUBJECT = "Invitation from %s for the CVN"
+INVITE_TEXT = "Join Democracy Club to help out in your constituency for the upcoming election!"
+INVITE_SUBJECT = "Invitation from %s for Democracy Club"
 
 INVITE_NOTICE_SUCCESS = "Invitation(s) sent. Thanks!"
 
