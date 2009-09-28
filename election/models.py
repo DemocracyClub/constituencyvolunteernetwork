0247b87b65a0376ac1d35beeb316c566c05a618d
tom <tom@tomdunham.org>
2009-09-28 12:31:39 +0000

    Added Candidate model

--- election/models.py
+++ election/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from signup.models import Constituency
+
 from slugify import smart_slugify
 
 class Party(models.Model):
@@ -20,3 +22,22 @@ class Party(models.Model):
             self.slug = smart_slugify(self.name)
         super(Party, self).save(*args, **kwargs)
 
+
+class Candidate(models.Model):
+    """
+    A Candidate stands in a Constituency, possibly reprisenting a Party.
+    """
+    name = models.CharField(max_length=80)
+    slug = models.SlugField(max_length=80)
+    address = models.TextField(max_length=300, blank=True)
+    email = models.EmailField(blank=True)
+    party = models.ForeignKey(Party, blank=True)
+    constituency = models.ForeignKey(Constituency)
+
+    def __unicode__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if self.slug == "":
+            self.slug = smart_slugify(self.name)
+        super(Candidate, self).save(*args, **kwargs)
--- election/tests.py
+++ election/tests.py
@@ -4,10 +4,11 @@ unittest). These will both pass when you run "manage.py test".
 
 Replace these with more appropriate tests for your application.
 """
+import datetime
 
 from django.test import TestCase
 
-from election.models import Party
+from election.models import Party, Constituency, Candidate
 
 class TestModels(TestCase):
     def test_party_slug(self):
@@ -15,3 +16,11 @@ class TestModels(TestCase):
         p = Party(name="Monster Raving Looney")
         p.save()
         self.assertEquals("Monster-Raving-Looney", p.slug)
+
+    def test_cand_slug(self):
+        # candidate auto-slugs
+        p = Party.objects.create(name="Foo")
+        con = Constituency.objects.create(name="Bar", year=datetime.date(2009,1,1))
+        c = Candidate.objects.create(name="Will Fibble", party=p, constituency=con)
+        self.assertEquals("Will-Fibble", c.slug)
+        
