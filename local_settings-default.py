129a9cf3e72852bdc2cff4854f3254c26b532429
francis <francis@flourish.org>
2009-12-15 11:13:23 +0000

    Put GMail settings from Wiki here as example

--- local_settings-default.py
+++ local_settings-default.py
@@ -1,4 +1,17 @@
+# Local settings file for Democracy Club website
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+DEFAULT_FROM_EMAIL = "democracyclub@localhost"
+
+# If you have a local SMTP server with no authentication for local connections
 EMAIL_HOST = "localhost"
-DEFAULT_FROM_EMAIL = "seb.bacon@gmail.com"
+
+# If you want to use GMail's SMTP server
+#EMAIL_HOST = "smtp.gmail.com"
+#EMAIL_HOST_USER = "you@gmail.com"
+#EMAIL_HOST_PASSWORD = "your_password"
+#EMAIL_USE_TLS = True
+#EMAIL_PORT = 587
+
