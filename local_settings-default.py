bd530a3bf82db11d5672db41d911c91a16675d58
francis <francis@flourish.org>
2009-12-15 11:23:28 +0000

    Put database settings here, as example.

--- local_settings-default.py
+++ local_settings-default.py
@@ -1,8 +1,18 @@
 # Local settings file for Democracy Club website
 
+# Debug settings
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+# Database settings
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'volnet'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Email settings
 DEFAULT_FROM_EMAIL = "democracyclub@localhost"
 
 # If you have a local SMTP server with no authentication for local connections
