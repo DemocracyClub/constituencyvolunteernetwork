0aece6b6d0ba9518421bc07018f159c501786893
seb.bacon <seb.bacon@220e2412-9961-11de-a05d-3bd73e269465>
2010-01-20 16:23:30 +0000

    nicer invite completion process, fixes Issue 39

--- invite/views.py
+++ invite/views.py
@@ -1,8 +1,6 @@
 from urllib import quote
 
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
@@ -10,6 +8,7 @@ from django.contrib.sites.models import Site
 
 from forms import InviteForm
 from signup.views import render_with_context
+from utils import addToQueryString
 
 @login_required
 def index(request):
@@ -18,8 +17,9 @@ def index(request):
         invite_form = InviteForm(request.POST, request.FILES)
         if invite_form.is_valid():
             invite_form.save(request.user)
-
-            return HttpResponseRedirect(reverse('home'))
+            context = {'notice':'Thanks for inviting more people to join!'}
+            return HttpResponseRedirect(addToQueryString(reverse('tasks'),
+                                                         context))
         else:
             vars['invite_form'] = invite_form
     else:
