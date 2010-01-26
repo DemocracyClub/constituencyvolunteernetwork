797581822c5b15418d0105fdac5f447323087d0e
seb.bacon <seb.bacon@220e2412-9961-11de-a05d-3bd73e269465>
2010-01-26 13:19:05 +0000

    support touching users based on constituency

--- tasks/management/commands/task.py
+++ tasks/management/commands/task.py
@@ -5,9 +5,49 @@ from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
 
 from signup.models import CustomUser
+from signup.models import Constituency
 from signup.signals import user_touch
 from tasks.models import TaskUser
 from tasks.models import Task
+import settings
+from signup import twfy
+
+class ConstituencyUserChain(object):
+    def __init__(self, *constituencies):
+        self.constituencies = self._flatten(*constituencies)
+        self.current_idx = 0
+        self.current_user_idx = 0
+        self._set_current()
+        
+    def _set_current(self):
+        self.current = self.constituencies[self.current_idx]\
+                       .customuser_set.filter(is_active=True).all()
+        
+    def _flatten(self, *items):
+        flattened = []
+        for item in items:
+            if isinstance(item, type([])):
+                flattened.extend(item)
+            else:
+                flattened.append(item)
+        return flattened
+
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        try:
+            user = self.current[self.current_user_idx]
+            self.current_user_idx += 1
+            return user
+        except IndexError:
+            try:
+                self.current_idx += 1
+                self._set_current()
+                self.current_user_idx = 0
+                return self.next()
+            except IndexError:
+                raise StopIteration
 
 class Command(BaseCommand):
     option_list =  BaseCommand.option_list + (
@@ -17,6 +57,15 @@ class Command(BaseCommand):
         make_option('--slug', '-s', dest='task_slug',
                     action="store",
                     help="Only assign named task"),
+        make_option('--max-number', '-m', dest='max_number',
+                    action="store",
+                    help="Maximum number of constituencies to consider"),
+        make_option('--max-distance', '-d', dest='max_distance',
+                    action="store",
+                    help="Maximum distance of constituencies to consider"),
+        make_option('--centre-postcode', '-p', dest='near_postcode',
+                    action="store",
+                    help="Assign constituency-wise in order of distance from postcode"),
         make_option('--emailfilter', '-e', dest='emailfilter',
                     action="store",
                     help="Only assign to users with emails containing")
@@ -35,7 +84,34 @@ class Command(BaseCommand):
             raise CommandError("Please specify a command, e.g. 'touch' or 'email'")
         if args[0] not in ('touch','email'):
             raise CommandError("Unknown command '%s'" % args[0])
-        users = CustomUser.objects.all()
+        near_postcode = options.get('near_postcode', None)
+        if not near_postcode:
+            has_distance = options['max_distance']
+            has_number = options['max_number']
+            
+            if has_distance and not has_number:
+                raise CommandError("'max-distance' option requires a "
+                                   "postcode")
+            if has_number and not has_distance:
+                raise CommandError("'max-number' option requires a "
+                                   "postcode")
+            
+        if hasattr(options, 'max_distance') and not near_postcode:
+            raise CommandError("'max-distance' option requires a postcode")
+        max_distance = int(options['max_distance'] or 500)
+        max_number = int(options['max_number'] or 100)
+        if near_postcode:
+            constituencies = []
+            constituency_name = twfy.getConstituency(near_postcode)
+            constituency = Constituency.objects.all()\
+                           .filter(name=constituency_name)\
+                           .get(year=settings.CONSTITUENCY_YEAR)
+            neighbours = constituency.neighbors(limit=max_number,
+                                                within_km=max_distance)
+            users = ConstituencyUserChain(constituency, neighbours)
+
+        else:
+            users = CustomUser.objects.all()
         emailfilter = options.get('emailfilter', '')
         if emailfilter:
             users = users.filter(email__icontains=emailfilter)
