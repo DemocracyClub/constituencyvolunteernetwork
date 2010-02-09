6a6ab188a647d326ab04f15840762f59bc561caf
francis <francis@flourish.org>
2010-02-09 18:14:21 +0000

    Status for an issue

--- issue/models.py
+++ issue/models.py
@@ -4,6 +4,12 @@ from django.conf import settings
 from tasks.models import Task
 from signup.models import Constituency, CustomUser
 
+STATUS_CHOICES = (
+    ('new', 'New'),
+    ('approved', 'Approved'),
+    ('junk', 'Junk'),
+)
+
 class Issue(models.Model):
     question = models.TextField()
     reference_url = models.URLField(max_length=2048, # reasonable maximum: http://www.boutell.com/newfaq/misc/urllength.html
@@ -14,6 +20,9 @@ class Issue(models.Model):
 
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    status = models.TextField(max_length=1, choices=STATUS_CHOICES)
+
 
     def __unicode__(self):
         return self.question
