130e634f93327393c5b6e60f04d83e4543985a72
francis <francis@flourish.org>
2010-02-10 10:06:22 +0000

    Fix this to work from this directory (for my own local deployment - am guessing Seb isn't using it on the live site as there is no directory called "mysite")

--- wsgi_handler.py
+++ wsgi_handler.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
        
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
