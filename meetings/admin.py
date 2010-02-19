b1f869b2fa98eab56f842b35540203fe7e198f57
seb.bacon <seb.bacon@220e2412-9961-11de-a05d-3bd73e269465>
2010-02-19 18:39:18 +0000

    ...and a bit more admin interface for meetings

--- meetings/admin.py
+++ meetings/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 import models
 
 class MeetingInterestAdmin(admin.ModelAdmin):
-    search_fields = ('user__email', 'postcode')
+    list_display = ('user', 'postcode', 'organiser', 'date')
+    list_filter = ('organiser',)
+    search_fields = ('user__email','postcode')
 
 admin.site.register(models.MeetingInterest, MeetingInterestAdmin)
