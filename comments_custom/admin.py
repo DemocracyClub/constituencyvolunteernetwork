d28dd120dd05ac04893f24ef4f7d74d91d68ef46
timothy.green <timothy.green@220e2412-9961-11de-a05d-3bd73e269465>
2010-03-18 15:14:24 +0000

    Improve comment notification admin

--- comments_custom/admin.py
+++ comments_custom/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 
 import models
 
-
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('submit_date', 'user', 'comment', 'is_public', 'removal_reason')
     list_filter = ('is_public', 'is_removed')
-    search_fields = ('user__email',)    
+    search_fields = ('user__email',)
+
+class NotifyCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'constituency', 'notify_type', 'created_at')
+    list_filter = ('notify_type',)
+    search_fields = ('user__email', 'constituency__name')    
 
 admin.site.register(models.CommentSimple, CommentAdmin)
-admin.site.register(models.NotifyComment)
+admin.site.register(models.NotifyComment, NotifyCommentAdmin)
 
 
 
