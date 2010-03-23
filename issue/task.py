dc664554a238789e47618c4a2cfa4865be663bf6
timothy.green <timothy.green@220e2412-9961-11de-a05d-3bd73e269465>
2010-03-23 20:33:46 +0000

    slight rewording to reflect how we're pitching issues
    tidy line lengths
    
    
    duplicate task code for second round of emailing
    
    
    remove unused import
    
    
    Questions list formatting and ux
    
    
    Out of date text on questions page
    
    
    Added ge2010 hashtag to issue tweets

--- issue/task.py
+++ issue/task.py
@@ -83,3 +83,74 @@ user_join_constituency.connect(callback_assign)
 # Completion signals
 issue_added.connect(callback_issue_added)
 
+###################
+task_slug = "describe-local-issues-2"
+
+@task_completion(task_slug)
+def callback_issue_added_2(sender, **kwargs):
+    """
+        Set this task as complete if they've added one issue.
+    """
+    user = kwargs['user']
+    task = kwargs['task']
+    task_user = kwargs['task_user']
+    issue = kwargs['issue']
+
+    # Only do the task once?
+    if task_user.state != TaskUser.States.completed:
+        task_user.complete()
+
+    try:
+        badge = Badge.objects.get(task=task, user=user)
+        match = re.search(r"Added (.+) local issue",
+                          badge.name).group(1)
+        if match == 'a':
+            name = "Added 2 local issues"
+        else:
+            name = "Added %d local issues" % (int(match) + 1)
+        badge.name = name
+        badge.save()
+    except Badge.DoesNotExist:
+        badge = Badge.objects.create(name="Added a local issue",
+                                     task=task,
+                                     user=user)
+
+
+@task_assign(task_slug)
+def callback_assign_2(sender, **kwargs):
+    """
+        Assign this task to everyone who activates the signals and isn't
+        already doing the task.
+    """
+    user = kwargs['user']
+    task = kwargs['task']
+
+    constituencies = None
+    if "constituencies" in kwargs and kwargs['constituencies'] is not None:
+        constituencies = kwargs['constituencies']
+    else:
+        constituencies = user.current_constituencies
+
+    current_site = Site.objects.get_current()
+    assigned = []
+    for constituency in constituencies:
+        kwargs = {'constituency': constituency.slug}
+        add_issue_url = reverse("add_issue", kwargs=kwargs)
+        try:                
+            TaskUser.objects.assign_task(task,
+                                         user,
+                                         add_issue_url,
+                                         constituency=constituency)
+        except TaskUser.AlreadyAssigned:
+            msg = "%s already assigned to %s in %s"
+            assigned.append(msg % (task, user, constituency))
+    return "; ".join(assigned)
+
+# Assignment signals
+user_activated.connect(callback_assign_2)
+user_touch.connect(callback_assign_2)
+user_join_constituency.connect(callback_assign_2)
+
+# Completion signals
+issue_added.connect(callback_issue_added_2)
+
