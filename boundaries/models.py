fc8dff9c045713555deb0ca6554dd86313851c92
martin.speleo <martin.speleo@220e2412-9961-11de-a05d-3bd73e269465>
2010-03-25 01:14:37 +0000

    First step towards removing geodjango dependancies

--- boundaries/models.py
+++ boundaries/models.py
@@ -1,15 +1,11 @@
-from django.contrib.gis.db import models
+from django.db import models
 from signup.models import Constituency
 
 class Boundary(models.Model):
     constituency = models.ForeignKey(Constituency)
-    boundary = models.PolygonField(srid=900913)
-    objects = models.GeoManager()
+    boundary = models.TextField()
     zoom = models.IntegerField()
-
-#To find what constituencies where clicked on
-#qs = Boundary.objects.filter(boundary__poly__contains='POINT(-104.590948 38.319914)')
-
-#To find what constituencies need rendering in a particular tile
-#tileGeom = GEOSGeometry('POLYGON (( 10 10, 10 20, 20 20, 20 15, 10 10))')
-#qs = Boundary.objects.filter(poly__bboverlaps=tileGeom)
+    north = models.FloatField()
+    south = models.FloatField()
+    east = models.FloatField()
+    west = models.FloatField()
--- boundariesdata/load.py
+++ boundariesdata/load.py
@@ -4,6 +4,8 @@ from boundaries.models import Boundary
 from django.contrib.gis.geos import Point
 maxZoom = 10
 
+google_dist = 20037508.34
+
 for f in ['boundariesdata/england.kml', 
           'boundariesdata/wales.kml', 
           'boundariesdata/scotland.kml', 
@@ -16,32 +18,47 @@ for f in ['boundariesdata/england.kml',
         v = []
         for coords in place.getElementsByTagName("coordinates"):
             points = []
+            north = - google_dist
+            south = google_dist
+            east = - google_dist
+            west = google_dist
             for coord in coords.childNodes[0].toxml().split(" "):
                 s = coord.split(",")
                 if len(s) == 3:
                     x, y = [float(c) for c in coord.split(",")][:2]
                     p = Point(x, y, srid=4326)
                     p.transform(900913)
-                    points.append(p.coords)
+                    gx, gy = p.coords
+                    if gy > north: north = gy
+                    if gy < south: south = gy                    
+                    if gx > east: east = gx
+                    if gx < west: west = gx
+                    points.append((gx, gy))
             print "points", len(points)
             for z in range(maxZoom + 1):
-                pixelsize2 = ((40075016.68 / 256) / (2 ** z)) ** 2
+                pixelsize2 = ((2 * google_dist / 256) / (2 ** z)) ** 2
                 u = []
                 previousX = 1e20
                 previousY = 1e20
                 for x, y in points:
                     if z == maxZoom:
-                        u.append("%f %f" % (x, y))
+                        u.append("(%f, %f)" % (x, y))
                     elif (x - previousX) ** 2 + (y - previousY) ** 2 > pixelsize2:
-                        u.append("%f %f" % (x, y))
+                        u.append("(%f, %f)" % (x, y))
                         previousX, previousY = x, y
                 if z != maxZoom and (previousX, previousY) != (x, y):
-                    u.append("%f %f" % (x, y))
+                    u.append("(%f, %f)" % (x, y))
                 print len(u)
                 if len(u) > 3:
                     constituency = Constituency.objects.get(name = name)
-                    boundary="SRID=900913;POLYGON((%s))" % reduce(lambda x, y: "%s, %s" %(x, y), u).strip()
-                    b=Boundary(zoom = z, constituency = constituency, boundary=boundary)
+                    boundary="[%s]" % reduce(lambda x, y: "%s, %s" %(x, y), u).strip()
+                    b=Boundary(zoom = z, 
+                               constituency = constituency, 
+                               boundary=boundary,
+                               east = east,
+                               west = west,
+                               north = north,
+                               south = south)
                     try:
                         b.save()
                     except: 
