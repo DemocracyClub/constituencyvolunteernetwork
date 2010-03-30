a066f0128f26adaa8bb4fc533ca1bfde807ee1e1
seb.bacon <seb.bacon@220e2412-9961-11de-a05d-3bd73e269465>
2010-03-30 11:31:36 +0000

    hacked in a second task for a separate email campaign

--- ynmp/task.py
+++ ynmp/task.py
@@ -61,3 +61,55 @@ user_touch.connect(callback_assign)
 # Completion signals
 ynmp_action_done.connect(callback_ynmp_done)
 
+####################
+
+task_slug = "ynmp-details2"
+
+@task_completion(task_slug)
+def callback_ynmp_done2(sender, **kwargs):
+    """
+        Set this task as complete when some sufficient amount of ynmp work is done
+    """
+    user = kwargs['user']
+    task = kwargs['task']
+    task_user = kwargs['task_user']
+    ynmp_action = kwargs['ynmp_action']
+    
+    if task_user.state != TaskUser.States.completed:
+        task_user.complete()
+    
+    try:
+        badge = Badge.objects.get(task=task, user=user)
+        badge.number += 1
+        badge.save()
+    except Badge.DoesNotExist:
+        badge = Badge.objects.create(name="Added details on YNMP",
+                                     task=task,
+                                     user=user)
+
+@task_assign(task_slug)
+def callback_assign2(sender, **kwargs):
+    """
+        Assign this task to everyone who activates the signals and isn't
+        already doing the task.
+    """
+    user = kwargs['user']
+    task = kwargs['task']
+    url = ynmp_login_url(user, "bad_details")
+    msg = ""
+
+    try:                
+        TaskUser.objects.assign_task(task,
+                                     user,
+                                     url)
+    except TaskUser.AlreadyAssigned:
+        msg = "%s already assigned to %s" % (task, user)
+    return msg
+
+# Assignment signals
+# user_activated.connect(callback_assign)
+user_touch.connect(callback_assign2)
+
+# Completion signals
+ynmp_action_done.connect(callback_ynmp_done2)
+
