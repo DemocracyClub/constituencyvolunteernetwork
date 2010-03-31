ce259c2fc6d86269517baf3788e981597195d898
timothy.green <timothy.green@220e2412-9961-11de-a05d-3bd73e269465>
2010-03-31 10:23:10 +0000

    Extra bits for issue admin

--- issue/admin.py
+++ issue/admin.py
@@ -10,7 +10,14 @@ class IssueAdmin(VersionAdmin):
                     'created_by',
                     'status')
     list_filter = ('status',)
-    search_fields = ('question', 'created_by__email')
+    search_fields = ('question', 'created_by__email','last_updated_by',)
+
+class RefinedIssueAdmin(admin.ModelAdmin):
+    list_display = ('question',
+                    'constituency',
+                    'moderator',)
+    search_fields = ('question', 'moderator',)
 
 admin.site.register(models.Issue, IssueAdmin)
-admin.site.register(models.RefinedIssue)
+admin.site.register(models.RefinedIssue, RefinedIssueAdmin)
+
